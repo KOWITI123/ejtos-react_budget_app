2a3b3c1bdba7f67a4adfd7af2f57cabd308b1629
KOWITI123 <allan.kowiti@student.moringaschool.com>
2023-10-04 13:24:31 -0400

    Complete budget app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,61 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
+    const { budget, expenses, currency, dispatch } = useContext(AppContext); // Destructure dispatch from the context
+    const [inputValue, setInputValue] = useState(budget);
+
+    useEffect(() => {
+        // Check the input value after it has been updated
+        const parsedValue = parseFloat(inputValue);
+        const upperLimit = 20000;
+
+        if (!isNaN(parsedValue)) {
+            if (parsedValue < 0 || parsedValue > upperLimit) {
+                alert(`Budget must be between 0 and ${currency}${upperLimit}`);
+            } else if (parsedValue < budget) {
+                alert('You cannot reduce the budget value lower than the starting budget');
+            }
+        } else {
+            // Allow empty input (no value)
+            setInputValue("");
+        }
+    }, [inputValue, budget, currency, dispatch]); // Include dispatch in the dependency array
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        // Update the input value as the user types
+        setInputValue(event.target.value);
     }
+
+    const handleCurrencyChange = (event) => {
+        const newCurrency = event.target.value;
+
+        // Dispatch an action to update the currency
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: newCurrency
+        });
+    }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <div className="budget-container">
+                <span className="budget-label">Budget:</span>
+                <span className="budget-amount">{currency}</span>
+                <input type="number" step="10" min="0" value={inputValue} onChange={handleBudgetChange} />
+            </div>
+            <div className="currency-container">
+                <label>Currency:</label>
+                <select value={currency} onChange={handleCurrencyChange}>
+                    <option value="" disabled>Select Currency</option>
+                    <option value="$">Dollar</option>
+                    <option value="£">Pound</option>
+                    <option value="€">Euro</option>
+                    <option value="₹">Rupee</option>
+                </select>
+            </div>
+        </div>
     );
 };
+
 export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,9 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import './ExpenseItem.css'; // Import your custom CSS file for styling
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
-
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
-    };
+    const { dispatch, currency } = useContext(AppContext);
 
     const increaseAllocation = (name) => {
         const expense = {
@@ -20,17 +13,40 @@ const ExpenseItem = (props) => {
 
         dispatch({
             type: 'ADD_EXPENSE',
-            payload: expense
+            payload: expense,
         });
+    };
 
-    }
+    const reduceAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense,
+        });
+    };
+
+    const handleChangeCurrency = (cost) => {
+        return `${currency}${cost}`;
+    };
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>{handleChangeCurrency(props.cost)}</td>
+            <td>
+                <button className="custom-button" onClick={() => increaseAllocation(props.name)}>
+                    <div className="plus-sign">+</div>
+                </button>
+            </td>
+            <td>
+                <button className="custom-button minus-sign" onClick={() => reduceAllocation(props.name)}>
+                    <div>-</div>
+                </button>
+            </td>
         </tr>
     );
 };
